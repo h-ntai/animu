5932eb038ea89f0299b4cbdd3ae9621c19fb8070
Türkischer Sniper <96152215+turkish-sniper@users.noreply.github.com>
2022-01-20 17:00:00 +0100

    Update script.js

--- hug/script.js
+++ hug/script.js
@@ -1,19 +1,10 @@
 /* image paths have been defined into a javascript list */
 var images = [
-    "img/1.jpg",
-    "img/2.jpg",
-    "img/3.jpg",
-    "img/4.jpg",
-    "img/5.jpg",
-    "img/6.jpg",
-    "img/7.jpg",
-    "img/8.jpg",
-    "img/9.jpg",
-    "img/10.jpg",
-    "img/11.jpg",
-    "img/12.jpg",
-    "img/13.jpg",
-    "img/14.jpg"];
+    "img/1.png",
+    "img/2.png",
+    "img/3.png",
+    "img/4.png",
+    "img/5.png"];
 
 /* quotes have been defined into a javascript list */
 
